b50b24334093a03f534d09ad4fd516ac35280f21
MarcRoemmelt <mail@m-roemmelt.com>
2020-04-09 02:21:15 +0200

    feat: create basic media upload service

---
+++ src/modules/workers/media/mediaUpload.ts
@@ -0,0 +1,62 @@
+/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-console */
+import fs from 'fs';
+import cloudinary from 'cloudinary';
+import { config } from 'config';
+import { Business, Image } from 'persistance/models';
+
+const cloud = cloudinary.v2;
+cloud.config({
+  cloud_name: config.cloudinary.cloudName,
+  api_key: config.cloudinary.apiKey,
+  api_secret: config.cloudinary.apiSecret,
+});
+
+function uploadImage(path: string): void {
+  cloud.uploader.upload(path, async (_error: any, result: { [key: string]: string }) => {
+    // create Image db Entry;
+    const fileName = path.split('.')[0].split('/').pop() as string;
+    const businessId = fileName.split('_')[1];
+    const imageType = fileName.split('_')[2];
+    const image = await Image.create({
+      title: businessId,
+      src: result.url,
+      publicId: result.public_id,
+    });
+    const business = await Business.findOne({ id: businessId });
+    if (!business) return;
+    if (imageType.includes('Logo')) {
+      business.media.logo = image._id;
+      await business.save();
+      console.log('saved as Logo');
+    }
+    if (imageType.includes('Hauptbild')) {
+      business.media.cover.image = image._id;
+      await business.save();
+    }
+    if (imageType.includes('Story')) {
+      const rank = parseInt(imageType.split('').slice(5).join(''), 10);
+      await image.save();
+      business.media.stories.images[rank] = image._id;
+      await business.save();
+    }
+  });
+}
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function mediaUpload(base = '/home/marcroemmelt/Downloads/upload'): Promise<void> {
+  fs.readdir(base, (_err, filenames) => {
+    for (const filename of filenames) {
+      const newPath = `${base}/${filename}`;
+      const exists = fs.existsSync(newPath);
+      if (!exists) continue;
+      const isDir = fs.lstatSync(newPath).isDirectory();
+      if (isDir) {
+        mediaUpload(newPath);
+        continue;
+      }
+      uploadImage(newPath);
+    }
+  });
+}
